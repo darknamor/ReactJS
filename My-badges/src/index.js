7ccb35335fc9fe64d5a1ad323a98558a4441154a
Nicolas Miranda <darknamor@gmail.com>
2020-01-27 18:38:53 -0300

    Avannces

--- My-badges/src/index.js
+++ My-badges/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react'; //analogo a create element
 import ReactDOM from 'react-dom'; //analogo a apend child
 
+const jsx = (<div>
+    <h1>Hola, soy Nicolas Miranda</h1>
+    <p>Soy ingeniero full stack.</p>
+</div>);
+//const element = React.createElement('h1',{},'Hola! soy los children');
 const container = document.getElementById('app');
 
-const element = <h1>Hello, My Badges</h1>;
-ReactDOM.render(element, container);
+ReactDOM.render(jsx, container);
