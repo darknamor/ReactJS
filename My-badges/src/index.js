03cbae88994a226877244f9ce9b5009cc43aa0e8
Nicolas Miranda <darknamor@gmail.com>
2020-01-28 12:00:44 -0300

    Algunas cosas mas

--- My-badges/src/index.js
+++ My-badges/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react'; //analogo a create element
 import ReactDOM from 'react-dom'; //analogo a apend child
 
-const jsx = (<div>
+const jsx = (
+  <div>
     <h1>Hola, soy Nicolas Miranda</h1>
     <p>Soy ingeniero full stack.</p>
-</div>);
+  </div>
+);
 //const element = React.createElement('h1',{},'Hola! soy los children');
 const container = document.getElementById('app');
 
