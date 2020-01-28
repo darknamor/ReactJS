d8abdb942bf7948df7252e575ea09e43298c7f32
Nicolas Miranda <darknamor@gmail.com>
2020-01-28 15:01:14 -0300

    Estilos

--- My-badges/src/componnets/Badje.js
+++ My-badges/src/componnets/Badje.js
@@ -1,23 +1,32 @@
 import React from 'react';
-import confLogo from '../images/badge-header.svg';
-
+import './Styles/Badge.css';
+import img1 from '../images/idea.svg';
+import img2 from '../images/coding.svg';
+import img3 from '../images/product.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 class Badge extends React.Component {
   render() {
     return (
-      <div>
-        <div>
-          <img src={confLogo} alt='Logo'></img>
+      <div className='Badge'>
+        <div className='Badge__header'>
+          <img src={img1} alt='Logo'></img>
+          <img src={img2} alt='Logo'></img>
+          <img src={img3} alt='Logo'></img>
         </div>
-        <div>
-          <img src='' alt='Avatar'></img>
+        <div className='Badge__section-name'>
+          <img
+            className='Badge__avatar'
+            src='https://es.gravatar.com/userimage/28968053/02e629a3077930841ed5341e32666afe?size=200'
+            alt='Avatar'></img>
           <h1>
             Nicolás <br /> Miranda
           </h1>
         </div>
-        <div>
-          <p>Full Stack Enginerr</p>
-          <p>@darknamor</p>
+        <div className='Badge__section-info'>
+          <h3>Full Stack Engineer</h3>
+          <div>@darknamor</div>
         </div>
+        <div className='Badge__footer'>#gato</div>
       </div>
     );
   }
--- My-badges/src/index.js
+++ My-badges/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'; //analogo a create element
 import ReactDOM from 'react-dom'; //analogo a apend child
 import Badge from './componnets/Badje';
-
+import './global.css'
 const container = document.getElementById('app');
 
 ReactDOM.render(<Badge />, container);
